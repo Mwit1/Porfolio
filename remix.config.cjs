39eef0f5607dc779bc550e32374c0a8b9b737fe1
Mwit1 <emwiti69@gmail.com>
2025-06-21 12:51:31 +0300

    Reset for Deployment

---
+++ remix.config.cjs
@@ -0,0 +1,21 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "build/index.js",
+  devServerPort: 8002,
+
+  // ✅ This is critical for Cloudflare Pages
+  output: "static",
+
+  future: {
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true,
+  },
+};
